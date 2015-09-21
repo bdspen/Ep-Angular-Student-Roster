b057cc8bcb20eec39c8d38d4595e015f708a79de
Ben Spenard & Mike Chastain <student@epicodus.com>
2015-09-21 10:53:27 -0700

    Add angular functionality up to addStudent method

---
+++ js/StudentsController.js
@@ -0,0 +1,8 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+    $scope.students = [];
+    $scope.addStudent = function(){
+
+        $scope.students.push({name: $scope.studentName });
+        $scope.studentName = null;
+    };
+});
