0a828ee15ff3b7e6e3efa8a239643671988bca77
Ben Spenard & Alexandra Brown <student@epicodus.com>
2015-09-22 11:03:58 -0700

    add permission slips to app. Lists students with or withou permission is seperate lists

--- controllers/FieldTripsController.js
+++ controllers/FieldTripsController.js
@@ -1,4 +1,5 @@
-studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope) {
+studentRoster.controller('FieldTripsCtrl', function FieldTripsCtrl($scope, StudentsFactory) {
+  $scope.students = StudentsFactory.students;
   $scope.addStudentWithPermissionSlip = function(student) {
     student.permissionSlip = true;
   };
